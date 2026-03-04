4e4fb49faf8e1f493347ae084e909205add39958
NIYOMUGISHA Obed <obed6@duck.com>
2026-03-04 15:46:17 +0200

    feat: add file download functionality and improve page structure

--- main.tsx
+++ main.tsx
@@ -4,11 +4,17 @@ import { Page } from './utils.tsx';
 
 const app = new Hono();
 
+app.get('/file/*', async (c) => {
+  const relative = c.req.path.replace(/^\/file/, '');
+  const file = await Deno.open('.' + relative);
+  return c.body(file.readable);
+});
+
 app.get('/*', async (c) => {
   const path = c.req.path;
   const entries = await listFilesAndDir('.' + path);
 
-  return c.html(<Page filesAndDirs={entries} base={c.req.url}></Page>);
+  return c.html(<Page filesAndDirs={entries} base={path}></Page>);
 });
 
 Deno.serve(app.fetch);
--- utils.tsx
+++ utils.tsx
@@ -15,28 +15,39 @@ interface Props {
   base: string;
 }
 
-export const Page: FC<Props> = ({ filesAndDirs, base }) => (
-  <>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>FTP - Server</title>
-      </head>
-      <body>
-        <a href="/">Home</a>
-        <h2>{base}</h2>
-        <ul>
-          {filesAndDirs.map(({ name, isDirectory, isFile }) => {
-            return (
-              <li key={name}>
-                {isFile && `📄 ${name}`}
-                {isDirectory && <a href={`${base}/${name}`}>📁 {name}</a>}
-              </li>
-            );
-          })}
-        </ul>
-      </body>
-    </html>
-  </>
-);
+export const Page: FC<Props> = ({ filesAndDirs, base }) => {
+  const hrefBase = base === '/' ? '' : base;
+
+  return (
+    <>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>FTP - Server</title>
+        </head>
+        <body>
+          <a href="/">Home</a>
+          <h2>{base}</h2>
+          <ul>
+            {filesAndDirs.map(({ name, isDirectory, isFile }) => {
+              return (
+                <li key={name}>
+                  {isFile && (
+                    <a download={name} href={`/file${hrefBase}/${name}`}>
+                      📄 {name}
+                    </a>
+                  )}
+                  {isDirectory && <a href={`${hrefBase}/${name}`}>📁 {name}</a>}
+                </li>
+              );
+            })}
+          </ul>
+        </body>
+      </html>
+    </>
+  );
+};
